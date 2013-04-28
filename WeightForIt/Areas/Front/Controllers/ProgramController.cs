05662f3bb1b8a4f945decb29a78b79576d76aa41
dygcao <dygcao@gmail.com>
2013-04-28 23:27:43 +0200

    Add basic controller for Program in Area Front

---
+++ WeightForIt/Areas/Front/Controllers/ProgramController.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using WeightForIt.Models;
+
+namespace WeightForIt.Areas.Front.Controllers
+{
+    public class ProgramController : Controller
+    {
+        private WfiEntities db = new WfiEntities();
+
+        //
+        // GET: /Front/Program/
+
+        public ActionResult Index()
+        {
+            var programs = db.Programs.Include(p => p.UserProfile);
+            return View(programs.ToList());
+        }
+
+        //
+        // GET: /Front/Program/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Program program = db.Programs.Find(id);
+            if (program == null)
+            {
+                return HttpNotFound();
+            }
+            return View(program);
+        }
+
+        //
+        // GET: /Front/Program/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.UserId = new SelectList(db.UserProfiles, "UserId", "UserName");
+            return View();
+        }
+
+        //
+        // POST: /Front/Program/Create
+
+        [HttpPost]
+        public ActionResult Create(Program program)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Programs.Add(program);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.UserId = new SelectList(db.UserProfiles, "UserId", "UserName", program.UserId);
+            return View(program);
+        }
+
+        //
+        // GET: /Front/Program/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Program program = db.Programs.Find(id);
+            if (program == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.UserId = new SelectList(db.UserProfiles, "UserId", "UserName", program.UserId);
+            return View(program);
+        }
+
+        //
+        // POST: /Front/Program/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Program program)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(program).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.UserId = new SelectList(db.UserProfiles, "UserId", "UserName", program.UserId);
+            return View(program);
+        }
+
+        //
+        // GET: /Front/Program/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Program program = db.Programs.Find(id);
+            if (program == null)
+            {
+                return HttpNotFound();
+            }
+            return View(program);
+        }
+
+        //
+        // POST: /Front/Program/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Program program = db.Programs.Find(id);
+            db.Programs.Remove(program);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ WeightForIt/Areas/Front/Views/Program/Edit.aspx
@@ -0,0 +1,84 @@
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/Views/Shared/Site.Master" Inherits="System.Web.Mvc.ViewPage<WeightForIt.Models.Program>" %>
+
+<asp:Content ID="Content1" ContentPlaceHolderID="TitleContent" runat="server">
+    Edit
+</asp:Content>
+
+<asp:Content ID="Content2" ContentPlaceHolderID="MainContent" runat="server">
+
+<h2>Edit</h2>
+
+<% using (Html.BeginForm()) { %>
+    <%: Html.ValidationSummary(true) %>
+
+    <fieldset>
+        <legend>Program</legend>
+
+        <%: Html.HiddenFor(model => model.ProgramId) %>
+
+        <div class="editor-label">
+            <%: Html.LabelFor(model => model.objective) %>
+        </div>
+        <div class="editor-field">
+            <%: Html.EditorFor(model => model.objective) %>
+            <%: Html.ValidationMessageFor(model => model.objective) %>
+        </div>
+
+        <div class="editor-label">
+            <%: Html.LabelFor(model => model.StartDate) %>
+        </div>
+        <div class="editor-field">
+            <%: Html.EditorFor(model => model.StartDate) %>
+            <%: Html.ValidationMessageFor(model => model.StartDate) %>
+        </div>
+
+        <div class="editor-label">
+            <%: Html.LabelFor(model => model.privacy) %>
+        </div>
+        <div class="editor-field">
+            <%: Html.EditorFor(model => model.privacy) %>
+            <%: Html.ValidationMessageFor(model => model.privacy) %>
+        </div>
+
+        <div class="editor-label">
+            <%: Html.LabelFor(model => model.UserId, "UserProfile") %>
+        </div>
+        <div class="editor-field">
+            <%: Html.DropDownList("UserId", String.Empty) %>
+            <%: Html.ValidationMessageFor(model => model.UserId) %>
+        </div>
+
+        <div class="editor-label">
+            <%: Html.LabelFor(model => model.PublicSince) %>
+        </div>
+        <div class="editor-field">
+            <%: Html.EditorFor(model => model.PublicSince) %>
+            <%: Html.ValidationMessageFor(model => model.PublicSince) %>
+        </div>
+
+        <div class="editor-label">
+            <%: Html.LabelFor(model => model.label) %>
+        </div>
+        <div class="editor-field">
+            <%: Html.EditorFor(model => model.label) %>
+            <%: Html.ValidationMessageFor(model => model.label) %>
+        </div>
+
+        <p>
+            <input type="submit" value="Save" />
+        </p>
+    </fieldset>
+<% } %>
+
+<div>
+    <%: Html.ActionLink("Back to List", "Index") %>
+</div>
+
+</asp:Content>
+
+<asp:Content ID="Content3" ContentPlaceHolderID="FeaturedContent" runat="server">
+</asp:Content>
+
+<asp:Content ID="Content4" ContentPlaceHolderID="ScriptsSection" runat="server">
+    <%: Scripts.Render("~/bundles/jqueryval") %>
+</asp:Content>
