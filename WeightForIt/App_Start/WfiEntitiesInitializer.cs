3761c7d1b9eb972ad1b3479dc0ec829c9a777c14
WilfriedDeluche <wilfried.deluche@gmail.com>
2013-04-28 21:24:20 +0200

    Fixes links namespace

--- WeightForIt/App_Start/WfiEntitiesInitializer.cs
+++ WeightForIt/App_Start/WfiEntitiesInitializer.cs
@@ -13,6 +13,7 @@ namespace WeightForIt.Models
         {
             new List<Food>
             {
+                // Fruits
                 new Food { FoodId = 1, RefValue = "Abricot", unit = "Unité", calories = 14, proteins = 0, glucides = 3, lipids = 0 },
                 new Food { FoodId = 2, RefValue = "Ananas", unit = "dag (100g)", calories = 50, proteins = 0, glucides = 12, lipids = 0 },
                 new Food { FoodId = 3, RefValue = "Banane", unit = "Unité", calories = 116, proteins = 1, glucides = 27, lipids = 0 },
@@ -22,7 +23,17 @@ namespace WeightForIt.Models
                 new Food { FoodId = 7, RefValue = "Kiwi", unit = "dag (100g)", calories = 53, proteins = 2, glucides = 11, lipids = 0 },
                 new Food { FoodId = 8, RefValue = "Melon", unit = "dag (100g)", calories = 30, proteins = 1, glucides = 6, lipids = 0 },
                 new Food { FoodId = 8, RefValue = "Mandarine", unit = "Unité", calories = 45, proteins = 1, glucides = 11, lipids = 0 },
-                new Food { FoodId = 8, RefValue = "Mangue", unit = "dag (100g)", calories = 63, proteins = 0, glucides = 15, lipids = 0 }
+                new Food { FoodId = 8, RefValue = "Mangue", unit = "dag (100g)", calories = 63, proteins = 0, glucides = 15, lipids = 0 },
+                new Food { FoodId = 8, RefValue = "Myrtilles", unit = "dag (100g)", calories = 71, proteins = 1, glucides = 16, lipids = 0 },
+                new Food { FoodId = 8, RefValue = "Orange", unit = "dag (100g)", calories = 45, proteins = 1, glucides = 9, lipids = 0 },
+                new Food { FoodId = 8, RefValue = "Pamplemousse", unit = "dag (100g)", calories = 66, proteins = 1, glucides = 16, lipids = 0 },
+                new Food { FoodId = 8, RefValue = "Pêche", unit = "dag (100g)", calories = 46, proteins = 1, glucides = 11, lipids = 0 },
+                new Food { FoodId = 8, RefValue = "Poire", unit = "Unité", calories = 107, proteins = 1, glucides = 26, lipids = 0 },
+                new Food { FoodId = 8, RefValue = "Pomme", unit = "Unité", calories = 80, proteins = 0, glucides = 19, lipids = 0 },
+                new Food { FoodId = 8, RefValue = "Prune", unit = "dag (100g)", calories = 52, proteins = 1, glucides = 12, lipids = 0 },
+                new Food { FoodId = 8, RefValue = "Pruneaux", unit = "dag (100g)", calories = 234, proteins = 2, glucides = 54, lipids = 0 },
+                new Food { FoodId = 8, RefValue = "Raisin", unit = "dag (100g)", calories = 69, proteins = 1, glucides = 15, lipids = 1 },
+                new Food { FoodId = 8, RefValue = "Rhubarbe", unit = "dag (100g)", calories = 18, proteins = 0, glucides = 4, lipids = 0 }
             }.ForEach(i => context.Foods.Add(i));
         }
     }
