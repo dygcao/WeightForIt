aa98b84cff910f88bb666ee99b9c03ac92ddae4b
Nicolas Bernadine <nicolas.bernadine@gmail.com>
2013-04-29 23:03:28 +0200

    Adds the FeedController - First branch commit

---
+++ WeightForIt/Areas/Front/Controllers/FeedController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeightForIt.Areas.Front.Controllers
+{
+    public class FeedController : Controller
+    {
+        //
+        // GET: /Front/Feed/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
