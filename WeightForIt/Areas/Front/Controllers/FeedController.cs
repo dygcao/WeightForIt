2926dc9717b2fa7894426915321dc8a30f84c51d
Nicolas Bernadine <nicolas.bernadine@gmail.com>
2013-05-01 15:01:04 +0200

    Adds the partial for the Program Feed

--- WeightForIt/Areas/Front/Controllers/FeedController.cs
+++ WeightForIt/Areas/Front/Controllers/FeedController.cs
@@ -3,18 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using WeightForIt.Models;
 
 namespace WeightForIt.Areas.Front.Controllers
 {
     public class FeedController : Controller
     {
+        private WfiEntities db = new WfiEntities();
         //
         // GET: /Front/Feed/
 
         public ActionResult Index()
         {
-            return View();
+            var programFeed = db.Programs.OrderByDescending(p => p.PublicSince).Where(p => p.privacy == 0).Take(10);
+          // Hook up or initialize _db here however you normally are doing it
+
+          return PartialView(programFeed);
         }
+
 
     }
 }
