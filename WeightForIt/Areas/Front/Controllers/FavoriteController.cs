49d7205e47cdb04340678e552edd5a156605ff1c
Nicolas Bernadine <nicolas.bernadine@gmail.com>
2013-05-02 16:39:21 +0200

    Corrects the Favorites view

--- WeightForIt/Areas/Front/Controllers/FavoriteController.cs
+++ WeightForIt/Areas/Front/Controllers/FavoriteController.cs
@@ -19,11 +19,9 @@ namespace WeightForIt.Areas.Front.Controllers
 
         public ActionResult Index()
         {
-            var MyId = WebSecurity.CurrentUserId;
+            myId = WebSecurity.CurrentUserId;
 
             var favoritePrograms = db.Favorites.Where(f => f.UserId == myId).ToList();
-            //var favoritePrograms = db.Programs.Include() OrderByDescending(p => p.PublicSince).Where(p => p.ProgramId == favoriteList.ProgramId).ToList();
-
             ViewData["favoritePrograms"] = favoritePrograms;
             return View(favoritePrograms);
         }
